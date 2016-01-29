0ef9067bb480151fbcfd2ed1952563fac26564ab
Jay Whang <jaywhang83@gmail.com>
2016-01-29 08:33:46 -0800

    add sect test for size of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 function Pizza (pizzaSize) {
-  this.pizzaSize = pizzaSize; 
+  this.pizzaSize = pizzaSize;
+};
+Pizza.prototype.sizePrice = function (sizeOfPizza) {
+  var priceforSizeOfPizza = 0;
+  if (sizeOfPizza === "large") {
+    //priceforSizeOfPizza = 15;
+    return 15;
+  };
+  //return priceforSizeOfPizza;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,10 @@
 describe("Pizza", function(){
-  it("returns the size of the pizza", function() {
+  it("returns the size of a pizza", function() {
     var testPizza = new Pizza("small");
     expect(testPizza.pizzaSize).to.equal("small");
+  });
+  it("returns a price of a pizza depend on the size", function () {
+    var testPizza = new Pizza("large");
+    expect(testPizza.sizePrice("large")).to.equal(15);
   });
 });
