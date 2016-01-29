722d62f11ea79a05d5f4fbb6c7a1978fe28adb96
Jay Whang <jaywhang83@gmail.com>
2016-01-29 09:53:04 -0800

    pass second test for topping

--- spec/specs.js
+++ spec/specs.js
@@ -24,6 +24,6 @@ describe("Toppings", function () {
   });
   it("returns a toppings choosen by uer", function() {
     var testTopping = new Toppings("canadian bacon");
-    expect(testTopping.toppingPrice("canadian bacon")).to.equal(["canadian bacon"]);
+    expect(testTopping.toppingPrice("canadian bacon")).to.eql(["canadian bacon"]);
   })
 });
